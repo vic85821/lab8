a3557b39455f0b028398db63754552109425f9fa
student <F74036297@2015cpp.(none)>
2015-06-16 17:44:49 +0800

    finish

--- MinoT.h
+++ MinoT.h
@@ -2,7 +2,6 @@
 #define MINO_T_H
 #include <iostream>
 #include "Mino.h"
-
 using namespace std;
 class MinoT: public Mino{
     public:
--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 #include <cstdlib>
 #include <ctime>
 #include "genMino.h"
+#include<new>
 using namespace std;
+void my_handler()
+{
+    cerr<<"error new Mino pointer"<<endl;
+    exit(1);
+}
 int main()
 {
     Mino * mino_ptr;
+    set_new_handler(my_handler);
     srandom(time(NULL));
     for(int i=0;i<3;++i)
     {
